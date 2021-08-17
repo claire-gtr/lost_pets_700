14edb0c16255a558e19ab96f11af78f0120e3276
claire gautier <claire.gautier@edhec.com>
2021-08-17 18:02:36 +0200

    finished livecode

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pet.destroy_all
+puts "Destroyed all pets"
+Pet.create(name: "Snoopy", species: "dog", address: "Amsterdam", date: Date.today - 1)
+Pet.create(name: "Garfield", species: "cat", address: "Brussels", date: Date.today - 5)
+Pet.create(name: "Kungfu", species: "panda", address: "Stockholm", date: Date.today - 4)
+Pet.create(name: "Remi", species: "rat", address: "Amsterdam", date: Date.today - 3)
+Pet.create(name: "Prince", species: "frog", address: "Brussels", date: Date.today - 4)
